195da2d86df305537e7e67d4662f05ab3ddba9c6
unknown <kevinmz@umich.edu>
2020-04-25 22:43:58 -0400

    changed more settings.py and requirements.txt

--- django_posts/settings.py
+++ django_posts/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,4 +141,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'kevinzhao69'
 EMAIL_HOST_PASSWORD = 'nsbvwjksxttjcylh'
 
-
+django_heroku.settings(locals())
