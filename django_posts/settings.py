4d3c4e8a9ab2bcb6a1f23a0b32b87b43a6a97e44
unknown <kevinmz@umich.edu>
2020-04-30 04:06:15 -0400

    update

--- django_posts/settings.py
+++ django_posts/settings.py
@@ -145,7 +145,8 @@ EMAIL_HOST_PASSWORD = 'nsbvwjksxttjcylh'
 django_heroku.settings(locals())
 
 AWS_ACCESS_KEY_ID = 'AKIAR76ERFVG3HOWGEVT'
-AWS_SECRET_ACCESS_KEY = '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'
+val = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = val
 AWS_STORAGE_BUCKET_NAME = 'wolverine-dm-files'
 
 AWS_S3_FILE_OVERWRITE = False
